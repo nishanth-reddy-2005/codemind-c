fe113fa84ce57412f19490643478ad58de103748
nishanth-reddy-2005 <119839756+nishanth-reddy-2005@users.noreply.github.com>
2022-12-30 00:34:33 +0530

    #include<stdio.h> int main() {     int n,a=0,b=0;     scanf("%d",&n);     for(int i=1;i<=n+1;i++)     {         if(i%2==0)         {             a=(a*3)+2;             printf("%d ",a);         }         else         {             b=(b*2)+1;             printf("%d ",b);         }     } }

--- series.c
+++ series.c
@@ -1,24 +1,19 @@
 #include<stdio.h>
-#include<math.h>
-
 int main()
 {
-    int n,a,b,k1=0,k2=0;
+    int n,a=0,b=0;
     scanf("%d",&n);
-    printf("0 ");
-    for(int i=0;i<=n-2;i++)
+    for(int i=1;i<=n+1;i++)
     {
         if(i%2==0)
         {
-            a=pow(3,k1);
+            a=(a*3)+2;
             printf("%d ",a);
-            k1++;
         }
         else
         {
-            b=pow(2,k2);
+            b=(b*2)+1;
             printf("%d ",b);
-            k2++;
         }
     }
 }

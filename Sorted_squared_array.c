479ef5724b8083dc8a344ad1d6fdd79342ad59b2
nishanth-reddy-2005 <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-01-23 23:52:46 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        scanf("%d",&n);
        int x[n];
        for(int i=0;i<n;i++)
        {
            scanf("%d",&x[i]);
            if(x[i]<0)
            {
                x[i]*=-1;
            }
        }
        int t;
        for(int i=0;i<n;i++)
        {
            for(int j=i+1;j<n;j++)
            {
                if(x[i]>x[j])
                {
                    t=x[i];
                    x[i]=x[j];
                    x[j]=t;
                }
            }
        }
        for(int i=0;i<n;i++)
        {
            printf("%d ",x[i]*x[i]);
        }
    }

---
+++ Sorted_squared_array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+        if(x[i]<0)
+        {
+            x[i]*=-1;
+        }
+    }
+    int t;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(x[i]>x[j])
+            {
+                t=x[i];
+                x[i]=x[j];
+                x[j]=t;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",x[i]*x[i]);
+    }
+}

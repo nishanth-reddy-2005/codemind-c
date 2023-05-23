0fb04babf2f59d513dc93f8838a82c38c267eede
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-05-23 10:34:16 +0530

    #include <stdio.h> int main() {     int num1, num2, i, gcd;     scanf("%d%d",&num1,&num2);     for(i=1; i<=num1&&i<=num2;++i)     {         if(num1%i==0 && num2%i==0)             gcd = i;     }     printf("%d",gcd);     return 0; }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int num1,num2,i,gcd;
+    int num1, num2, i, gcd;
     scanf("%d%d",&num1,&num2);
-    for(i=1;i<=num1&&i<=num2;i++)
+    for(i=1; i<=num1&&i<=num2;++i)
     {
-        if(num1%i==0&&num2%i==0)
-        gcd=i;
+        if(num1%i==0 && num2%i==0)
+            gcd = i;
     }
     printf("%d",gcd);
     return 0;

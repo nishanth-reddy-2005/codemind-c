e5ef96b5e3e1cf7df2654af6f1ac9b0c383f7c92
P Nishanth Reddy <119839756+nishanth-reddy-2005@users.noreply.github.com>
2023-08-18 20:22:24 +0530

    #include<stdio.h> int rev(int); int main() {     int n,a,b,f,z;     scanf("%d",&n);     a=n/1000;     b=n%1000;     f=rev(b);     z=a*1000+f;     printf("%d",z); } int rev(int n) {     int r,x=0;     while(n!=0)     {         r=n%10;         x=x*10+r;         n=n/10;     }     return x; }

--- Reverse_Last_3_digits.c
+++ Reverse_Last_3_digits.c
@@ -1,22 +1,22 @@
-#include<stdio.h>
-int rev(int);
-int main()
+#include<stdio.h> 
+int rev(int); 
+int main() 
 {
-    int n,a,b,f,z;
-    scanf("%d",&n);
-    a=n/1000;
-    b=n%1000;
-    f=rev(b);
-    z=a*1000+f;
+    int n,a,b,f,z; 
+    scanf("%d",&n); 
+    a=n/1000; 
+    b=n%1000; 
+    f=rev(b); 
+    z=a*1000+f; 
     printf("%d",z);
 }
 int rev(int n)
 {
-    int r,x=0;
-    while(n!=0)
+    int r,x=0; 
+    while(n!=0) 
     {
         r=n%10;
-        x=x*10+r;
+        x=x*10+r; 
         n=n/10;
     }
     return x;
